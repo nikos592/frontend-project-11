0d7a368a06e8a7aa0bce08a5cacde9928f756ae9
Илья Никулин <wixestrees@gmail.com>
2024-10-26 21:43:31 +0300

    try hosting

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
